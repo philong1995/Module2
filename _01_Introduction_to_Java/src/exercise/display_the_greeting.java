c735402f6faf16124c01966028f1f80461da9193
philong1995 <68836316+philong1995@users.noreply.github.com>
2020-09-09 03:42:40 +0700

    module2

---
+++ _01_Introduction_to_Java/src/exercise/display_the_greeting.java
@@ -0,0 +1,11 @@
+package exercise;
+import java.util.Scanner;
+public class display_the_greeting {
+    public static void main(String[] args) {
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Hello: " + name);
+    }
+}
---
+++ _01_Introduction_to_Java/src/practice/systemTime.java
@@ -0,0 +1,9 @@
+package practice;
+import java.util.Date;
+public class systemTime {
+    public static void main(String[] args){
+        Date now = new Date();
+
+        System.out.println("Now is: "+now);
+    }
+}
