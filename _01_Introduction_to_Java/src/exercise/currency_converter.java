a69f10b40cd8d94ce5315a813644215c5722a2d1
philong1995 <68836316+philong1995@users.noreply.github.com>
2020-09-09 08:19:21 +0700

    module2

---
+++ _01_Introduction_to_Java/src/exercise/currency_converter.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.util.Scanner;
+
+public class currency_converter {
+
+    public static void main(String[] args) {
+        double USD;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your number USD: ");
+        USD = scanner.nextDouble();
+        double converter = USD*23000;
+        System.out.println("The value is converted from USD to VND: "+converter);
+    }
+}
